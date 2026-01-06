d51be788b0feba67c98f08fe35120d7669b13577
tariq-ou <tariq.eltayeb@hotmail.com>
2026-01-06 21:20:19 +0000

    Initial set up and ui additions

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecipeListComponent } from './components/recipe-list/recipe-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RecipeListComponent ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
