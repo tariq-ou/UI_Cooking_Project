0d194bc142f6ba44ab0178a670f70bd6ea576dc2
tariq-ou <tariq.eltayeb@hotmail.com>
2026-01-14 09:26:35 +0000

    Addes Add recipe page - more work needed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import {RouterModule, Routes} from '@angular/router';
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home';
 import { RecipeListComponent } from './components/recipe-list/recipe-list';
+import {RecipeNew} from './components/recipe-new/recipe-new';
+
 
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // root path
   { path: 'recipes', component: RecipeListComponent },
+  { path: 'add-recipe', component: RecipeNew },
 ];
